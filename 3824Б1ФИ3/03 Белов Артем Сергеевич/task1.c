ddfa9929fef96f4a9e3f8e7f600e8ec498b6e030
sadako16 <CCC.Art@yandex.com>
2024-12-05 09:49:52 +0300

    Update task1.c

--- 3824Б1ФИ3/03 Белов Артем Сергеевич/task1.c
+++ 3824Б1ФИ3/03 Белов Артем Сергеевич/task1.c
@@ -3,5 +3,7 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
+  if ((a+b) >= INT_MAX) return INT_MAX;
+  if else ((a+b) <= INT_MIN) return INT_MIN;
   return 0;
 }
